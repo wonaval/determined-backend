0a744482a2eb2e90418ef0ebe46408b71bac8a64
wonaval <wonaval@gmail.com>
2022-01-15 17:43:36 -0500

    Update d0dcb13251b1_create_routines.py

--- migrations/versions/d0dcb13251b1_create_routines.py
+++ migrations/versions/d0dcb13251b1_create_routines.py
@@ -23,7 +23,7 @@ def upgrade():
     op.create_table(
         'routines',
         sa.Column('id', sa.Integer, primary_key=True),
-        sa.Column('name', sa.String, unique=True, nullable=False),
+        sa.Column('name', sa.String, nullable=False),
         sa.Column('type', sa.String),
         sa.Column('user_id', sa.Integer),
 
